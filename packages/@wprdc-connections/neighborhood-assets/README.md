849b0d5b0b9e58ca58f95b282fe4eaa9806e968b
Steve Saylor <steven.saylor@pitt.edu>
2022-09-03 13:36:39 -0400

    removed asset connection

--- packages/@wprdc-connections/neighborhood-assets/README.md
+++
@@ -1,3 +0,0 @@
-# @wprdc-connections/neighborhood-assets
-
-React components for making civic tech and open data applications.
--- packages/@wprdc-connections/neighborhood-assets/src/hooks.tsx
+++
@@ -1,6 +0,0 @@
-import * as AssetsAPI from './api';
-import { useQuery } from 'react-query';
-
-export function useAssetDetails(assetID: number) {
-  return useQuery(['asset', assetID], () => AssetsAPI.getAssetById(assetID));
-}
